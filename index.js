2b0688c8bc18512d036e81ff0207084da9bde45d
jiithin <jithijithin9@gmail.com>
2024-10-26 03:45:17 +0530

    cors updated

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: ["http://localhost:5173"], credentials: true,}));
+app.use(cors({origin: ["https://divulge.onrender.com"], credentials: true,}));
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000!!');
