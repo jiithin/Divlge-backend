9fe31dcb97df44596d352a4056ad8b4354c8763b
jiithin <jithijithin9@gmail.com>
2024-10-26 00:28:36 +0530

    -

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+const cors = require("cors");
 import userRoutes from './Routes/userRoute.js';
 import authRoutes from './Routes/authRoute.js';
 import postRoutes from './Routes/postRoute.js';
@@ -20,7 +20,7 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: ["http://localhost:5173"], credentials: true,}));
+app.use(cors());
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000!!');
