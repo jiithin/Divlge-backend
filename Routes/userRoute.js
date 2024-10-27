2c36d9b65374e178900a86f2c982162c09f91673
jiithin <jithijithin9@gmail.com>
2024-10-27 07:41:59 +0530

    rollback

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -6,9 +6,9 @@ import { verifyToken } from '../Utils/verifyUser.js';
 const router = express.Router();
 
 router.get('/test', test);
-router.put('/update/:userId', updateUser);
-router.delete('/delete/:userId', deleteUser);
+router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
-router.get('/getusers', getUsers);
+router.get('/getusers', verifyToken, getUsers);
 router.get('/:userId', getUser);
 export default router;
